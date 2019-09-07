2dda1645f5690c29dcff4ab1c782cefa25b969d3
slash128 <admin@slash128.com>
2019-09-07 11:09:09 -0700

    Basic game play functional

---
+++ DC28_Jack_Attack_Badge.ino
@@ -0,0 +1,287 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+//
+/*
+Name:       DC28_Jack_Attack_Badge.ino
+Created:	8/11/2019 9:25:15 PM
+Author:     slash128
+*/
+
+#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <EEPROM.h>
+#include "sprites.h"
+
+// Define User Types below here or use a .h file
+//
+
+// OLED display settings
+#define OLED_ADDRESS	0x3C
+#define SCREEN_WIDTH	128
+#define SCREEN_HEIGHT	64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+void initDisplay();
+void initIO();
+void initGame();
+void shipControl();
+void laserControl();
+void jackControl();
+void collisionControl();
+signed int backgroundControl(signed int shipMovX, signed int shipMovY);
+void centerText(const char *Text, unsigned char Y);
+void updateDisplay();
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// Global Variables and Defines
+//
+struct shipStruct ship;
+struct jackStruct laser;
+struct jackStruct jack;
+
+const int shipWidth = 8;
+const int shipHeight = 16;
+const int shipSpeed = 4;
+const int laserWidth = 8;
+const int laserHeight = 2;
+const int laserSpeed = 6;
+const int jackWidth = 16;
+const int jackHeight = 16;
+//const int jackSpeed = 1;
+const int explosionWidth = 16;
+const int explosionHeight = 8;
+const int bgrndWidth = 128;
+const int bgrndHeight = 64;
+
+signed int shipStartX = (SCREEN_WIDTH / 4);
+signed int shipStartY = ((SCREEN_HEIGHT / 2) - (shipHeight / 2));
+signed int jackPosX = SCREEN_WIDTH;
+signed int jackPosY = ((SCREEN_HEIGHT / 2) - (jackHeight / 2));
+signed int bgrndPosX = 0;
+signed int bgrndPosY = 0;
+
+boolean laserFired = false;
+
+
+unsigned int highScore = 0;
+unsigned int score = 0;
+unsigned int playerLives = 0;
+
+#define UP 4
+#define DOWN 5
+#define LEFT 6
+#define RIGHT 7
+#define FIRE 8
+#define EYELEFT 9
+#define EYERIGHT 10
+#define BUZZER 11
+
+#define ALIVE 0
+#define HIT 1
+#define DEAD 2
+
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+	initDisplay();
+	initIO();
+	initGame();
+	
+	EEPROM.get(0,highScore);
+	if(highScore==65535)  // new unit never written to
+	{
+		highScore=0;
+		EEPROM.put(0,highScore);
+	}
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+	shipControl();
+	laserControl();
+	jackControl();
+	collisionControl();
+	backgroundControl(ship.locX, ship.locY);
+	updateDisplay();
+}
+
+void initDisplay() {
+	// Initialize the OLED display
+	display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+}
+
+void initIO() {
+	// Initialize I/O
+	pinMode(UP, INPUT_PULLUP);
+	pinMode(DOWN, INPUT_PULLUP);
+	pinMode(LEFT, INPUT_PULLUP);
+	pinMode(RIGHT, INPUT_PULLUP);
+	pinMode(FIRE, INPUT_PULLUP);
+	pinMode(EYELEFT, OUTPUT);
+	pinMode(EYERIGHT, OUTPUT);
+	pinMode(BUZZER, OUTPUT);
+}
+
+void initGame() {
+	ship.locX = shipStartX;
+	ship.locY = shipStartY;
+	ship.status = ALIVE;
+	
+	laser.status = DEAD;
+	
+	jack.locX = jackPosX;
+	jack.locY = random(8, 58);
+	jack.speed = random(1, 5);
+	jack.status = ALIVE;
+}
+
+void shipControl() {
+	
+	if(ship.status == DEAD) {
+		ship.locX = shipStartX;
+		ship.locY = shipStartY;
+		ship.status = ALIVE;
+	}
+	
+	if(digitalRead(UP) == 0) {
+		ship.locY -= shipSpeed;
+		if(ship.locY < 0) {
+			ship.locY = 0;
+		}
+	}
+	
+	if(digitalRead(DOWN) == 0) {
+		ship.locY += shipSpeed;
+		if(ship.locY > (SCREEN_HEIGHT - (shipHeight/2))) {
+			ship.locY = (SCREEN_HEIGHT - (shipHeight/2));
+		}
+	}
+	
+	if(digitalRead(LEFT) == 0) {
+		ship.locX -= shipSpeed;
+		if(ship.locX < 0) {
+			ship.locX = 0;
+		}
+	}
+	
+	if(digitalRead(RIGHT) == 0) {
+		ship.locX += shipSpeed;
+		if(ship.locX > SCREEN_WIDTH) {
+			ship.locX = SCREEN_WIDTH;
+		}
+	}
+	
+	if((digitalRead(FIRE) == 0) && (laser.status == DEAD)) {
+		laser.status = ALIVE;
+		laser.locX = ship.locX;
+		laser.locY = ship.locY + (shipHeight/2);
+	}
+}
+
+void laserControl() {
+	if(laser.status == ALIVE) {
+		laser.locX += laserSpeed;
+		if(laser.locX > SCREEN_WIDTH) {
+			laser.status = DEAD;
+		}
+	}
+}
+
+void jackControl() {
+	if(jack.status == DEAD) {
+		jack.locX = SCREEN_WIDTH;
+		jack.locY = random(8, 58);
+		jack.speed = random(1, 5);
+		jack.status = ALIVE;
+	}
+	
+	if(jack.status == HIT) {
+		jack.status = DEAD;
+	}
+
+	if(jack.status == ALIVE) {
+		jack.locX -= jack.speed;
+		if(jack.locX < 0) {
+			jack.locX = SCREEN_WIDTH;
+			jack.locY = random(8, 58);
+			jack.speed = random(1, 5);
+			jack.status == DEAD;
+		}
+	}
+}
+
+void collisionControl() {
+	
+	// Laser and Jack Collision
+	if((laser.status == ALIVE) && (jack.status == ALIVE)) {
+		if((jack.locX <= (laser.locX + laserWidth)) && (laser.locX <= jack.locX)) {
+			if(((laser.locY + laserHeight) >= jack.locY) && (laser.locY <= (jack.locY + jackHeight))) {
+				laser.status = DEAD;
+				jack.status = HIT;
+				score += 10;
+			}
+		}
+	}
+	
+	// Ship and Jack Collision
+	if((ship.status == ALIVE) && (jack.status == ALIVE)) {
+		if((jack.locX <= (ship.locX + shipWidth)) && (ship.locX <= jack.locX)) {
+			if(((ship.locY + shipHeight) >= jack.locY) && (ship.locY <= (jack.locY + jackHeight))) {
+				ship.status = HIT;
+				jack.status = HIT;
+			}
+		}
+	}
+	
+}
+
+signed int backgroundControl(signed int shipMovX, signed int shipMovY) {
+	
+}
+
+void centerText(const char *Text, unsigned char Y)  {
+	// Centers text on screen
+	display.setCursor(int((float)(SCREEN_WIDTH)/2-((strlen(Text)*6)/2)),Y);
+	display.print(Text);
+}
+
+void updateDisplay() {
+	display.clearDisplay();
+	
+	centerText("Score    ", 0);
+	display.print(score);
+	
+	if(ship.status == ALIVE) {
+		display.drawBitmap(ship.locX, ship.locY, shipBMP, shipWidth, shipHeight, WHITE);
+	}
+
+	if(ship.status == HIT) {
+		display.drawBitmap(ship.locX, ship.locY, explosionBMP, explosionWidth, explosionHeight, WHITE);
+		ship.status = DEAD;
+	}
+	
+	if(laser.status == ALIVE) {
+		display.drawBitmap(laser.locX, laser.locY, laserBMP, laserWidth, laserHeight, WHITE);
+	}
+	
+	if(jack.status == ALIVE) {
+		display.drawBitmap(jack.locX, jack.locY, jackBMP, jackWidth, jackHeight, WHITE);
+	}
+	
+	if(jack.status == HIT) {
+		display.drawBitmap(jack.locX, jack.locY, explosionBMP, explosionWidth, explosionHeight, WHITE);
+	}
+	
+	display.drawBitmap(bgrndPosX, bgrndPosY, bgrndBMP, bgrndWidth, bgrndHeight, WHITE);
+	
+	display.display();
+}
---
+++ Release/SPI/SPI.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\labadmin\AppData\Local\Temp\VMBuilds\DC28_Jack_Attack_Badge\leonardo\Release\SPI\SPI.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src\SPI.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src\SPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\leonardo/pins_arduino.h
